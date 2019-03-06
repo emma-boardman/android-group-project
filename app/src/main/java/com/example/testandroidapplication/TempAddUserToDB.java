f05e5f627fe8fb686c85e569356bd2fc07837dbc
Emma Boardman <emma@emmaboardman.co.uk>
2019-03-06 10:10:28 +0000

    add code for AsyncTask

--- app/src/main/java/com/example/testandroidapplication/TempAddUserToDB.java
+++ app/src/main/java/com/example/testandroidapplication/TempAddUserToDB.java
@@ -1,10 +1,14 @@
 package com.example.testandroidapplication;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 
 import com.example.testandroidapplication.helper.HttpJsonParser;
 import com.example.testandroidapplication.helper.CheckNetworkStatus;
@@ -22,14 +26,12 @@ public class TempAddUserToDB extends AppCompatActivity {
     private static final String KEY_EMAIL = "Email";
     private static final String KEY_PASSWORD = "Password";
     private static final String BASE_URL = "http://40414669.wdd.napier.ac.uk/users";
-    private static String STRING_EMPTY = "";
     private EditText userNameEditText;
     private EditText userEmailEditText;
     private EditText userPasswordEditText;
     private String userName;
     private String userEmail;
     private String userPassword;
-    private Button addButton;
     private int success;
     private ProgressDialog pDialog;
 
@@ -42,6 +44,94 @@ public class TempAddUserToDB extends AppCompatActivity {
         userNameEditText = findViewById(R.id.txtUserNameAdd);
         userEmailEditText =  findViewById(R.id.txtUserEmailAdd);
         userPasswordEditText = findViewById(R.id.txtUserPasswordAdd);
-        addButton =  findViewById(R.id.addNewUserBtn);
+        Button addButton = findViewById(R.id.addNewUserBtn);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (CheckNetworkStatus.isNetworkAvailable(getApplicationContext())) {
+                    addUser();
+                } else {
+                    Toast.makeText(TempAddUserToDB.this,
+                            "Unable to connect to internet",
+                            Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+    }
+
+    private void addUser() {
+        String STRING_EMPTY = "";
+        if (!STRING_EMPTY.equals(userNameEditText.getText().toString()) &&
+                !STRING_EMPTY.equals(userEmailEditText.getText().toString()) &&
+                !STRING_EMPTY.equals(userPasswordEditText.getText().toString())) {
+
+            userName = userNameEditText.getText().toString();
+            userEmail = userEmailEditText.getText().toString();
+            userPassword = userPasswordEditText.getText().toString();
+            new AddUserAsyncTask().execute();
+        } else {
+            Toast.makeText(TempAddUserToDB.this,
+                    "One or more fields left empty!",
+                    Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private class AddUserAsyncTask extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //Display progress bar
+            pDialog = new ProgressDialog(TempAddUserToDB.this);
+            pDialog.setMessage("Adding User. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpJsonParser httpJsonParser = new HttpJsonParser();
+            Map<String, String> httpParams = new HashMap<>();
+            //Populating request parameters
+            httpParams.put(KEY_USER_NAME, userName);
+            httpParams.put(KEY_EMAIL, userEmail);
+            httpParams.put(KEY_PASSWORD, userPassword);
+            JSONObject jsonObject = httpJsonParser.makeHttpRequest(
+                    BASE_URL + "addUser.php", "POST", httpParams);
+            try {
+                success = jsonObject.getInt(KEY_SUCCESS);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String result) {
+            pDialog.dismiss();
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    if (success == 1) {
+                        //Display success message
+                        Toast.makeText(TempAddUserToDB.this,
+                                "User Added", Toast.LENGTH_LONG).show();
+                        Intent i = getIntent();
+                        //send result code 20 to notify about movie update
+                        setResult(20, i);
+                        //Finish ths activity and go back to listing activity
+                        finish();
+
+                    } else {
+                        Toast.makeText(TempAddUserToDB.this,
+                                "Some error occurred while adding movie",
+                                Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            });
+        }
+
     }
 }
