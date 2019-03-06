f8710e02722da617435a7222f72ad39c92778872
Emma Boardman <emma@emmaboardman.co.uk>
2019-03-06 09:14:18 +0000

    add temp add user activity, change layout to linear, add button to add new user

---
+++ app/src/main/java/com/example/testandroidapplication/TempAddUserToDB.java
@@ -0,0 +1,13 @@
+package com.example.testandroidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TempAddUserToDB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_add_user_to_db);
+    }
+}
