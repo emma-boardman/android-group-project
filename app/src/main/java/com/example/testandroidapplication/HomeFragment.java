62efef3b3f0a1cb0a88685aa0daa2e87785a71bd
buzzbomb <djinnes20@gmail.com>
2019-04-23 15:50:04 +0100

    homeFragment second draft
    created contentuseradapter
    
    created homeitemlist class
    
    following Cals method in ChatFragment

--- app/src/main/java/com/example/testandroidapplication/HomeFragment.java
+++ app/src/main/java/com/example/testandroidapplication/HomeFragment.java
@@ -1,6 +1,39 @@
 package com.example.testandroidapplication;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.testandroidapplication.Adapter.ContentUserAdapter;
+import com.example.testandroidapplication.Model.HomeItemList;
+import com.example.testandroidapplication.Model.User;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    private ContentUserAdapter contentUserAdapter;
+    private java.util.List<User> mUsers;
+
+    private List<HomeItemList> homeItems;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container,false);
+
+        recyclerView = v.findViewById(R.id.home_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return v;
+    }
 }
