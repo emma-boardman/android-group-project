77841b50f8e7d38420abb92c4550bd356898e5c0
Emma Boardman <emma@emmaboardman.co.uk>
2019-04-25 13:47:16 +0100

    remove test buttons from login and register fragment, reformat code

--- app/src/main/java/com/example/testandroidapplication/LoginFragment.java
+++ app/src/main/java/com/example/testandroidapplication/LoginFragment.java
@@ -27,9 +27,6 @@ public class LoginFragment extends Fragment {
 
     private EditText userEmailEditText;
     private EditText userPasswordEditText;
-    private String userEmail;
-    private String userPassword;
-    private int success;
     private RegisterFragment register = new RegisterFragment();
     private FirebaseAuth auth;
 
@@ -37,54 +34,30 @@ public class LoginFragment extends Fragment {
     @Override
     @TargetApi(26)
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_main,container, false);
+        View v = inflater.inflate(R.layout.activity_main, container, false);
 
         userEmailEditText = v.findViewById(R.id.txtUserNameLogin);
         userPasswordEditText = v.findViewById(R.id.txtUserPasswordLogin);
         auth = FirebaseAuth.getInstance();
-        Button loginBtn =  v.findViewById(R.id.loginBtn);
+        Button loginBtn = v.findViewById(R.id.loginBtn);
         Button addNewBtn = v.findViewById(R.id.addNewBtn);
-        Button btn_messaging = v.findViewById(R.id.btn_messaging);
-        Button btn_calender = v.findViewById(R.id.btn_calender);
-        Button btn_test = v.findViewById(R.id.btn_test);
 
-//        btn_test.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(getActivity(), ReferenceHttpAsyncTasksForUI.class);
-//                startActivity(i);
-//            }
-//        });
-
-        /*btn_messaging.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getActivity(), Messaging.class);
-                startActivity(i);
-            }
-        });*/
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                /*Intent i = new Intent(getActivity().getApplicationContext(),
-                        ContentActivity.class);
-                startActivity(i);
-
-                getActivity().finish();*/
-
                 final String txt_email = userEmailEditText.getText().toString();
                 String txt_password = userPasswordEditText.getText().toString();
 
-                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
                     Toast.makeText(getActivity(), "Please enter your email and password.", Toast.LENGTH_SHORT).show();
                 } else {
                     auth.signInWithEmailAndPassword(txt_email, txt_password)
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if(task.isSuccessful()) {
+                                    if (task.isSuccessful()) {
                                         Intent intent = new Intent(Objects.requireNonNull(getActivity()).getApplicationContext(),
                                                 ContentActivity.class);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -97,21 +70,6 @@ public class LoginFragment extends Fragment {
                                 }
                             });
                 }
-                // if network is available, validate the user input
-
-                ///
-                /// needs updating to work with Cals firebase Login
-                ///
-                /*
-                if (CheckNetworkStatus.isNetworkAvailable(getActivity().getApplicationContext())) {
-                    validateUser();
-
-                } else {
-                    //Display error message if not connected to internet
-                    Toast.makeText(getActivity(),
-                            "Unable to connect to internet",
-                            Toast.LENGTH_LONG).show();
-                }*/
             }
 
         });
@@ -124,9 +82,7 @@ public class LoginFragment extends Fragment {
 
                     getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragement_container,
                             register, "registerFragment").commit();
-                    /*Intent i = new Intent(getActivity().getApplicationContext(),
-                            ReferenceHttpAsyncTasksForUI.class);
-                    startActivity(i);*/
+
                 } else {
                     //Display error message if not connected to internet
                     Toast.makeText(getActivity(),
@@ -137,17 +93,7 @@ public class LoginFragment extends Fragment {
             }
         });
 
-        btn_calender.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent i = new Intent(getActivity(), CalendarFragment.class);
-                startActivity(i);
-
-            }
-        });
-
         return v;
     }
 
- }
+}
