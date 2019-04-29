fdc6f55a8680a6886619983e6a18280c2588cc39
benma <ben.maddy@hotmail.co.uk>
2019-04-29 11:32:02 +0100

    calendAr

--- app/src/main/java/com/example/testandroidapplication/NewGigFragment.java
+++ app/src/main/java/com/example/testandroidapplication/NewGigFragment.java
@@ -6,13 +6,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CalendarView;
-
 import android.widget.Toast;
 
 
